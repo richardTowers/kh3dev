db4257722d5184a3ca8a66676b0c2faee3a90605
richard_towers@yahoo.co.uk <richard_towers@yahoo.co.uk@92feb609-08b6-d527-3540-3c29148df28d>
2010-11-11 11:29:32 +0000

    Created a new project to practice sockets programming

---
+++ khepera/netTestClient/trunk/netTestClient.c
@@ -0,0 +1,13 @@
+/*****************************************************************/
+/*  netTestClient.c
+/*	Created by richard at 11:28 on 11/11/2010
+/*	Copyright Durham University 2010, all rights reserved
+/*
+/*****************************************************************/
+
+#include "netTestClient.h"
+
+int main(int argc, char * argv[])
+{
+	//Code goes here...
+}
---
+++ khepera/netTestClient/trunk/netTestClient.h
@@ -0,0 +1,10 @@
+/*****************************************************************/
+/*  netTestClient.h
+/*	Created by richard at 11:28 on 11/11/2010
+/*	Copyright Durham University 2010, all rights reserved
+/*
+/*****************************************************************/
+#ifndef INC_
+#define INC_
+//Code goes here...
+#endif //INC_
