f645b365e3b1514d93cd60f559e5188acc40d9e0
richard_towers@yahoo.co.uk <richard_towers@yahoo.co.uk@92feb609-08b6-d527-3540-3c29148df28d>
2010-11-11 11:29:57 +0000

    Created a new project to practice sockets programming

---
+++ supervisor/netTestHost/trunk/netTestHost.c
@@ -0,0 +1,13 @@
+/*****************************************************************/
+/*  netTestHost.c
+/*	Created by richard at 11:28 on 11/11/2010
+/*	Copyright Durham University 2010, all rights reserved
+/*
+/*****************************************************************/
+
+#include "netTestHost.h"
+
+int main(int argc, char * argv[])
+{
+	//Code goes here...
+}
