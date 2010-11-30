b7519ad4656371c348ab4880d292c6778eaf16f0
richard_towers@yahoo.co.uk <richard_towers@yahoo.co.uk@92feb609-08b6-d527-3540-3c29148df28d>
2010-11-30 10:51:54 +0000

    Created new project to monitor fitness

---
+++ supervisor/fitMonitor/trunk/fitMonitor.c
@@ -0,0 +1,13 @@
+/*****************************************************************/
+/*  fitMonitor.c
+/*	Created by richard at 10:51 on 30/11/2010
+/*	Copyright Durham University 2010, all rights reserved
+/*
+/*****************************************************************/
+
+#include "fitMonitor.h"
+
+int main(int argc, char * argv[])
+{
+	//Code goes here...
+}
---
+++ supervisor/fitMonitor/trunk/fitMonitor.h
@@ -0,0 +1,10 @@
+/*****************************************************************/
+/*  fitMonitor.h
+/*	Created by richard at 10:51 on 30/11/2010
+/*	Copyright Durham University 2010, all rights reserved
+/*
+/*****************************************************************/
+#ifndef INC_FITMONITOR_H
+#define INC_FITMONITOR_H
+//Code goes here...
+#endif //INC_FITMONITOR
