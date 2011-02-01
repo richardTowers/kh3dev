5097ebf10ec6b1cafd414a922f69e9c304bc8271
richard_towers@yahoo.co.uk <richard_towers@yahoo.co.uk@92feb609-08b6-d527-3540-3c29148df28d>
2011-02-01 11:27:03 +0000

    Moved template matching fitness monitor to trunk

--- supervisor/fitMonitor/trunk/fitMonitor.c
+++ supervisor/fitMonitor/trunk/fitMonitor.c
@@ -1,153 +1,213 @@
 /*****************************************************************/
-/*  fitMonitor.c
-/*	Created by richard at 10:51 on 30/11/2010
-/*	Copyright Durham University 2010, all rights reserved
-/*
+//  fitMonitor.c
+//	Created by richard at 10:51 on 30/11/2010
+//	Copyright Durham University 2010, all rights reserved
+//
 /*****************************************************************/
 
 #include "fitMonitor.h"
 
-#define CV_NO_BACKWARD_COMPATIBILITY
-
 
 int main( void )
 {
-    CvCapture* capture = 0;
-    IplImage *frame = 0, *image = 0;
-    int c;
-
-    capture = cvCaptureFromCAM(ANY_CAMERA);
-
-    if( !capture )
-    {
-        fprintf(stderr,"Could not initialize capturing...\n");
-        return -1;
-    }
-
-    printf( "Hot keys: \n"
-        "\tESC - quit the program\n");
-
-    cvNamedWindow( "fitMonitor", 1 );
-
-		
-    for(;;)
-    {
-        frame = cvQueryFrame( capture );
-        if( !frame ) break;
-
-        if( !image )
-        {
-            /* allocate all the buffers */
-            image = cvCreateImage( cvGetSize(frame), 8, 3 );
-            image->origin = frame->origin;
-        }
-
-        cvCopy( findLights(frame), image, 0 );
-        //cvCvtColor( image, hsv, CV_BGR2HSV );
-
-        cvShowImage( "fitMonitor", image );
-
-        c = cvWaitKey(5);
-        if( (char) c == 27 ) exit(0);
-    }
-
-    cvReleaseCapture( &capture );
-    cvDestroyWindow("fitMonitor");
-
-    return 0;
+	printf("Main window accepts keyboard and mouse input:\n"
+	"  ESC - Quit\n"
+	"  Mouse - Drag to Select Area\n"
+	"  Return - Confirm Area\n");
+	
+	mainCapture();
 }
 
-IplImage *findLights(IplImage *frame)
+void mainCapture(void)
 {
-	struct rtTarget redTarget = {{0,0},0,0,0,0}, greenTarget={{0,0},0,0,0,0}, blueTarget={{0,0},0,0,0,0};
-	//IplImage *theImage = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
+	CvCapture *capture = 0;
+	IplImage *frame = 0, *histogram = 0;
+	rtRobotPosition robots[NUMBER_ROBOTS];
+	int robot=0, roundel=0;
 	
-	//cvCopy(frame, theImage, 0);
-	
-	//Dilate to make the target bigger
-	cvDilate(frame, frame, NULL, 2);
-	
-	//Reduce resolution to make our job easier
-	frame = downsize4(frame);
-	
-	//Find the brightest pixels who's hue is red, green and blue
+	//Start capturing:
+	if( !(capture = cvCaptureFromCAM(ANY_CAMERA)))
 	{
-		int row, column;
-		
-		//Convert the image to HLS:
-		cvCvtColor(frame, frame, CV_BGR2HLS);
-
-		//Get the image data
-		int height     = frame->height;
-		int width      = frame->width;
-		int step       = frame->widthStep/sizeof(uchar);
-		int channels   = frame->nChannels;
-		uchar* data    = (uchar *)frame->imageData;
-		
-		//For each pixel
-		for(row=0; row<height; row++) //For each row
+		fprintf(stderr,"Could not initialize capturing...\n");
+		exit(1);
+	}
+	cvNamedWindow("Main");
+	cvSetMouseCallback("Main",mouseHandler,NULL);
+	
+	
+	while(robot<NUMBER_ROBOTS)
+	{
+		while(roundel<2)
 		{
-			for(column=0; column<width; column++)	//For each column
+			keyHandler();
+			frame = cvQueryFrame( capture );
+			if( !frame ) {fprintf(stderr,"\nError: Couldn\'t grab frame.\n"); exit(1);}
+
+			cvRectangle(frame, rectStart, rectEnd, RED, 1, CV_AA, 0);
+			theSemaphor=GOTRECT;
+			if(gotRect==YES)
 			{
-				if(RED && LIGHTNESS > redTarget.lightness)
+				gotRect=NO;
+				//Clear the frame
+				frame = cvQueryFrame( capture );
+				//Shrink the image
+				frame=downsize4(downsize4(frame));
+				cvSetImageROI(frame, fullRect);
+				cvShowImage("Main", frame);
+				
+				printf("\a/************************************/"
+				"\nYou should be looking at a small image of:"
+				"\nROBOT: %d\nROUNDLE: %d (1 front, 2 back)\n\nPress Return if you are happy with your selection\n"
+				"Press C to change your selection\n\n",robot+1, roundel+1);
+				
+				for(;;)
 				{
-					redTarget.position.x=column;
-					redTarget.position.y=row;
-					redTarget.hue=HUE;
-					redTarget.lightness=LIGHTNESS;
-					redTarget.saturation=SATURATION;
+					keyHandler();
+					theSemaphor=SELECTIONORWINDOW;
+					if(selectionMade)
+					{
+						selectionMade=NO;
+						if(roundel==0)
+						{
+							robots[robot].marks.front=cvCreateImage(cvGetSize(frame), frame->depth, frame->nChannels);
+							cvCopy(frame, robots[robot].marks.front, 0);
+							roundel++;
+							break;
+						}
+						else if(roundel==1)
+						{
+							robots[robot].marks.rear=cvCreateImage(cvGetSize(frame), frame->depth, frame->nChannels);
+							cvCopy(frame, robots[robot].marks.rear, 0);
+							roundel++;
+							robot++;
+							break;
+						}
+						else roundel=0;
+					}
+					if(windowNeedsReset)
+					{
+						windowNeedsReset=NO;
+						break;
+					}
 				}
-				else if(BLUE && LIGHTNESS > blueTarget.lightness)
-				{
-					blueTarget.position.x=column;
-					blueTarget.position.y=row;
-					blueTarget.hue=HUE;
-					blueTarget.lightness=LIGHTNESS;
-					blueTarget.saturation=SATURATION;
-				}
-				else if(GREEN && LIGHTNESS > greenTarget.lightness)
-				{
-					greenTarget.position.x=column;
-					greenTarget.position.y=row;
-					greenTarget.hue=HUE;
-					greenTarget.lightness=LIGHTNESS;
-					greenTarget.saturation=SATURATION;
-				}
+				cvResetImageROI(frame);
 			}
+			cvShowImage("Main", frame);
 		}
-		//Convert back to BGR:
-		cvCvtColor(frame, frame, CV_HLS2BGR);
-		
-		//Put markers on the image
-		cvCircle(frame, redTarget.position, 5, cvScalar(255,0,0,0),-1,8, 0);
-		cvCircle(frame, blueTarget.position, 5, cvScalar(0,0,255,0),-1,8, 0);
-		cvCircle(frame, greenTarget.position, 5, cvScalar(0,255,0,0),-1,8, 0);
-		
-		//Relay the coordinates of the targets
-		redPosition.x = (int)redTarget.position.x*XSCALE; redPosition.y = (int)redTarget.position.y*YSCALE;
-		greenPosition.x = (int)greenTarget.position.x*XSCALE; greenPosition.y = (int)greenTarget.position.y*YSCALE;
-		bluePosition.x = (int)blueTarget.position.x*XSCALE; bluePosition.y = (int)blueTarget.position.y*YSCALE;
+		roundel=0;
 	}
 	
-	return frame;
+	//At this point all of the targets have been acquired and we can get on with following them.
+	printf("\n/************************************/"
+	"\nTarget Images obtained, beginning tracking...\n");
+	CvPoint maxloc;
+	CvSize hSize;
+	theSemaphor=ESC_ONLY;
+	
+	//cvNamedWindow("Robot1");
+	//cvNamedWindow("Robot2");
+	for(;;)
+	{
+		keyHandler();
+		frame = cvQueryFrame( capture );
+		if( !frame ) {fprintf(stderr,"\nError: Couldn\'t grab frame.\n"); exit(1);}
+		frame=downsize4(downsize4(frame));
+		
+		for (robot = 0; robot < NUMBER_ROBOTS; robot += 1)
+		{
+			for (roundel = 0; roundel < 2; roundel += 1)
+			{
+				if(roundel==0)
+				{
+					hSize=cvSize(frame->width-robots[robot].marks.front->width+1,
+					frame->height-robots[robot].marks.front->height+1);
+					
+					histogram = cvCreateImage(hSize, IPL_DEPTH_32F,1);
+					cvMatchTemplate(frame, robots[robot].marks.front, histogram, CV_TM_CCOEFF);
+					cvNormalize( histogram, histogram, 1, 0, CV_MINMAX );
+					cvMinMaxLoc(histogram, NULL, NULL, NULL, &maxloc, 0);
+					maxloc=cvPoint(maxloc.x+robots[robot].marks.front->width/2, maxloc.y+robots[robot].marks.front->height/2);
+					cvCircle(frame, maxloc, (robots[robot].marks.front->width+robots[robot].marks.front->height)/4, cvAvg(frame,NULL), -1, CV_AA);
+					robots[robot].position=maxloc;
+					
+					
+				}
+				//else cvMatchTemplate(frame, robots[robot].marks.rear, histogram, CV_TM_SQDIFF);
+				
+			}
+		}
+		frame = cvQueryFrame( capture );
+		cvCircle(frame, robots[0].position, MARK_SIZE, YELLOW, LINE_WIDTH, CV_AA);
+		cvCircle(frame, robots[1].position, MARK_SIZE, RED, LINE_WIDTH, CV_AA);
+		cvShowImage("Main", frame);
+	}
+	
 }
 
+
+/*****************************************EVENT HANDLERS*********************************/
+void keyHandler(void)
+{
+	char keyPress;
+	keyPress=cvWaitKey(5);
+	switch(keyPress)
+        {
+        	case ESC:
+        		exit(0);
+        	case '\n':
+        		if(theSemaphor==GOTRECT)
+        		{
+        			if(rectStart.x!=rectEnd.x && rectStart.y!=rectEnd.y) gotRect=YES;
+        			else printf("Please select a robot\n");
+        		}
+        		else if(theSemaphor==SELECTIONORWINDOW)	selectionMade=YES;
+        		break;
+        	case 'c':
+        		if(theSemaphor==SELECTIONORWINDOW) windowNeedsReset=YES;
+        		break;
+        	default:
+        		break;
+        }
+}
+void mouseHandler(int event, int x, int y, int flags, void* param)
+{
+	switch(event){
+		case CV_EVENT_LBUTTONDOWN:
+			mouseDown=YES;
+			rectStart=cvPoint(x,y);
+			break;
+
+		case CV_EVENT_LBUTTONUP:
+			mouseDown=NO;
+			rectEnd=cvPoint(x,y);
+			fullRect=cvRect(min(rectStart.x, rectEnd.x),
+			min(rectStart.y,rectEnd.y),abs(rectEnd.x - rectStart.x),abs(rectEnd.y - rectStart.y) );
+			break;
+
+		case CV_EVENT_MOUSEMOVE:
+			if(mouseDown) rectEnd=cvPoint(x,y);
+			break;
+
+	}
+}
+
+/*****************************************IMAGE HANDLERS*********************************/
 IplImage * downsize4(IplImage * frame)
 {
-		int frameHeight, frameWidth;
-		frameHeight=frame->height;
-		frameWidth=frame->width;
-    //IplImage *texImage = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
-    IplImage *pyr = cvCreateImage(cvSize(frameWidth/2, frameHeight/2), IPL_DEPTH_8U, 3);
-    IplImage *pyr2 = cvCreateImage(cvSize(frameWidth/4, frameHeight/4), IPL_DEPTH_8U, 3);
-    
-    cvPyrDown(frame, pyr, IPL_GAUSSIAN_5x5);
-    cvPyrDown(pyr, pyr2, IPL_GAUSSIAN_5x5);
-    
-    cvResize(pyr2, frame, CV_INTER_NN);
-    
-    cvReleaseImage(&pyr);
-    cvReleaseImage(&pyr2);
-    
-    return frame;
+	int frameHeight, frameWidth;
+	frameHeight=frame->height;
+	frameWidth=frame->width;
+
+	IplImage *pyr = cvCreateImage(cvSize(frameWidth/2, frameHeight/2), IPL_DEPTH_8U, 3);
+	IplImage *pyr2 = cvCreateImage(cvSize(frameWidth/4, frameHeight/4), IPL_DEPTH_8U, 3);
+
+	cvPyrDown(frame, pyr, IPL_GAUSSIAN_5x5);
+	cvPyrDown(pyr, pyr2, IPL_GAUSSIAN_5x5);
+
+	cvResize(pyr2, frame, CV_INTER_NN);
+
+	cvReleaseImage(&pyr);
+	cvReleaseImage(&pyr2);
+
+	return frame;
 }
--- supervisor/fitMonitor/trunk/fitMonitor.h
+++ supervisor/fitMonitor/trunk/fitMonitor.h
@@ -1,18 +1,21 @@
 /*****************************************************************/
-/*  fitMonitor.h
-/*	Created by richard at 10:51 on 30/11/2010
-/*	Copyright Durham University 2010, all rights reserved
-/*
+//  fitMonitor.h
+//	Created by richard at 10:51 on 30/11/2010
+//	Copyright Durham University 2010, all rights reserved
+//
 /*****************************************************************/
 #ifndef INC_FITMONITOR_H
 #define INC_FITMONITOR_H
 
+//Includes:
 #include <stdio.h>
+#include <math.h>
 
 #include "cv.h"
 #include "highgui.h"
 
-
+//Defines:
+#define CV_NO_BACKWARD_COMPATIBILITY
 #define NUMBER_ROBOTS 2
 #define XSCALE 100/(float)width
 #define YSCALE 100/(float)height
@@ -20,30 +23,65 @@
 #define HUE data[row*step+column*channels+0]
 #define LIGHTNESS data[row*step+column*channels+1]
 #define SATURATION data[row*step+column*channels+2]
-#define RED (HUE > 90 && HUE < 150)
-#define BLUE (HUE < 30 || HUE > 150)
-#define GREEN (HUE > 30 && HUE < 90)
+#define ISRED (HUE > 90 && HUE < 150)
+#define ISBLUE (HUE < 30 || HUE > 150)
+#define ISGREEN (HUE > 30 && HUE < 90)
+#define BLUE cvScalar(255,0,0)
+#define RED cvScalar(0,0,255)
+#define GREEN cvScalar(0,255,0)
+#define GREY cvScalar(150,150,150)
+#define YELLOW cvScalar(0,255,255)
 #define AREA 16
 #define ANY_CAMERA -1
+#define YES 1
+#define NO 0
+#define UNKNOWN 0
+#define EXCL_RADIUS 2
+#define ROI_SIZE 200
+#define PI 3.14159
+#define MARK_SIZE 30
+#define LINE_WIDTH 2
+#define NULL_POINT cvPoint(0xffff,0xffff)
+#define WIDTH 635
+#define HEIGHT 466
+#define ESC 27
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
+#define GOTRECT 0
+#define SELECTIONORWINDOW 1
+#define MOUSEDOWN 2
+#define ESC_ONLY 3
 
-struct rtRobotPosition {
-	int theRobot;
-	CvPoint thePosition;
-};
 
-struct rtTarget {
+//Types:
+typedef struct rtFlags {
+	IplImage* front;
+	IplImage* rear;
+} rtFlags;
+
+typedef struct rtRobotPosition {
+	rtFlags marks;
+	char positionIsKnown;
 	CvPoint position;
-	int hue;
-	int lightness;
-	int saturation;
-	int count;
-};
+	double heading;
+} rtRobotPosition;
 
-IplImage *findLights(IplImage *frame);
+//Functions:
+void mainCapture(void);
+void keyHandler(void);
+void subCapture(IplImage* fullFrame, CvRect bounds, int robot);
+void mouseHandler(int event, int x, int y, int flags, void* param);
 IplImage * downsize4(IplImage * frame);
 
-CvPoint redPosition;
-CvPoint greenPosition;
-CvPoint bluePosition;
+//Globals:
+rtRobotPosition robots[NUMBER_ROBOTS];
+unsigned short int theSemaphor;
+volatile bool mouseDown=NO;
+volatile bool gotRect=NO;
+volatile bool selectionMade=NO;
+volatile bool windowNeedsReset=NO;
+
+CvPoint rectStart={0,0};
+CvPoint rectEnd={0,0};
+CvRect fullRect={0,0,0,0};
 
 #endif //INC_FITMONITOR
