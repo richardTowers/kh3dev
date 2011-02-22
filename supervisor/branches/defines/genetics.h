68a2b976c96753a76ae62d3a7e1a69395d707c76
richard_towers@yahoo.co.uk <richard_towers@yahoo.co.uk@92feb609-08b6-d527-3540-3c29148df28d>
2011-02-22 22:10:51 +0000

    Decreased Mutation probability by a factor of five.

--- supervisor/branches/defines/genetics.h
+++ supervisor/branches/defines/genetics.h
@@ -9,14 +9,15 @@
 
 #define POP_SIZE 10			//The number of individuals to be evaluated before reproduction
 #define GENERATIONS 100		//The number of generations to evaluate
-#define EVAL_TIME 30		//The max time to spend evaluating an individual
-#define SURVIVAL_BONUS 20	//The bonus an individual recieves for not crashing
+#define EVAL_TIME 2			//The max time to spend evaluating an individual
+#define SURVIVAL_BONUS 00	//The bonus an individual recieves for not crashing
 #define TOUR_SIZE 3			//The number of individuals to compete in a tournament
 
-#define pMUTATE 1000
+#define pMUTATE 5000
 #define pSET_ZERO 5
 #define pADD_WEIGHT 5
 #define pFLIP_NEG 5
+
 #define pCHANGE_WEIGHT 20
 #define pFLIP_THIS_BIT 100 
 
