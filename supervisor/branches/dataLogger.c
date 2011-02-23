cd78e7fa694592e946205af31d79d8da062599f3
richard_towers@yahoo.co.uk <richard_towers@yahoo.co.uk@92feb609-08b6-d527-3540-3c29148df28d>
2011-02-23 14:23:09 +0000

    Added a dataLogger c file to deal with necessary retructuring of fitMonitor

---
+++ supervisor/branches/dataLogger.c
@@ -0,0 +1,12 @@
+/*****************************************************************/
+//  dataLogger.c
+//	Created by Richard Towers at 14:21 on 23/02/2011
+//	Copyright Durham University 2011, all rights reserved
+//
+/*****************************************************************/
+
+
+int main(int argc, char * argv[])
+{
+	//Code goes here...
+}
---
+++ supervisor/branches/dataLogger.h
@@ -0,0 +1,10 @@
+/*****************************************************************/
+//  dataLogger.h
+//	Created by Richard Towers at 14:21 on 23/02/2011
+//	Copyright Durham University 2011, all rights reserved
+//
+/*****************************************************************/
+#ifndef INC_DATALOGGER
+#define INC_DATALOGGER
+//Code goes here...
+#endif //INC_DATALOGGER
