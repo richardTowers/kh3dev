e816e6eea83d26637427fb0c6a58c34162cc0385
richard_towers@yahoo.co.uk <richard_towers@yahoo.co.uk@92feb609-08b6-d527-3540-3c29148df28d>
2011-04-20 12:46:03 +0000

    Expanded MATLAB scripts ability to process data, worked around ordering bug in FitnessList shell script

--- scripts/FitnessPlot.m
+++ scripts/FitnessPlot.m
@@ -1,5 +1,20 @@
 [a,fit]=system('FitnessList');
 fit=textscan(fit','%f');
 fit=fit{1,1};
-
-plot(fit);
+clf;
+plot1=axes();
+plot(fit,'.-', 'color', [0.5 0.5 0.5]);
+xlabel('Individual'); ylabel('Fitness');
+hold on;
+fit2=fit(1:10*round(end/10-0.5));
+fit2=reshape(fit2,10,length(fit2)/10);
+% Mean:
+plot(10.*(1:length(mean(fit2)))-5, mean(fit2)', '.-', 'color', 'blue');
+% Max:
+plot(10.*(1:length(max(fit2)))-5, max(fit2)', '.-', 'color', 'green');
+% Min:
+plot(10.*(1:length(min(fit2)))-5, min(fit2)', '.-', 'color', 'red');
+set(plot1, 'XTick',0:10:length(fit),'XGrid', 'on');
+% Trend:
+trend=fit-detrend(fit);
+plot(trend, '--', 'color', [.5 .5 .5]);
